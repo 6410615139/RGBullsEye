3187f446d219c8701b6c5541bcf72ce9b6496635
Krit <6410615139@cn436>
2024-01-19 11:03:26 +0700

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Supakrit Nithikethkul on 19/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
